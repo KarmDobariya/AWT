8db6a83f95b1db5ae50ba0176656c6cc833800ef
KarmDobariya <karmdobariya2006@gmail.com>
2026-02-04 13:43:33 +0530

    Update first.js

--- LAB/Lab-01/first.js
+++ LAB/Lab-01/first.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
         <title>My First Node Page</title>
       </head>
       <body>
-        <h1>Hello from Node.js 🚀</h1>
+        <h1>Hello from Node.js </h1>
         <p>This HTML is written inside Node</p>
       </body>
     </html>
